0f315a1a1bcbb11c34c9367c3bb8a75c4f35dac6
Christian Brabez <cbrabez@Christians-iMac.fritz.box>
2015-12-07 22:00:15 +0100

    initial commit

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# Comments hekp you understanding your program
+
+puts "I could have code like this." # The comment on the end of the line is ignored.
+
+# use a comment to disable a piece of code
+
+# puts "This won't run."
+
+puts "This will run."
